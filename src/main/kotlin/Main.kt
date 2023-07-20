dc0ed1643ef15f21391eda9088454ed3b06224c0
Gxrco <gersonrc111@icloud.com>
2023-07-20 07:24:22 -0600

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,6 +2,7 @@ fun main(args: Array<String>) {
     println("Hello World!")
 
     // Try adding program arguments via Run/Debug configuration.
+    //New commit
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
     println("Program arguments: ${args.joinToString()}")
 }
