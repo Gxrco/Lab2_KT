84ef132fe119205697f1267f3b6622f47bd3a230
Gxrco <gersonrc111@icloud.com>
2023-07-20 07:25:08 -0600

    Initial commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Hello World! changes")
 
     // Try adding program arguments via Run/Debug configuration.
     //New commit
